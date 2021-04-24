64e2a46b47f6d8a72bce137077ef330631861333
Ragiel Anugrah <Ragiel@macs-MacBook-Pro-4.local>
2021-04-24 13:18:00 +0700

    update wsgi

--- logintest/wsgi.py
+++ logintest/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhitenoise
+from whitenoise import Whitenoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logintest.settings')
 
 application = get_wsgi_application()
-application =  DjangoWhitenoise(application)
+application =  Whitenoise(application)
